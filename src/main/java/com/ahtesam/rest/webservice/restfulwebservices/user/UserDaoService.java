bf079faea3c782289392a8a191a5377119c80e2e
Ahtesam <137303420+AhtesamMuhammad@users.noreply.github.com>
2024-10-07 23:41:07 +0200

    Exception handling and delete user
    Inplement generic Exception handling and DELETE method to delete a user

--- src/main/java/com/ahtesam/rest/webservice/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/ahtesam/rest/webservice/restfulwebservices/user/UserDaoService.java
@@ -14,15 +14,15 @@ public class UserDaoService {
 
     private static List<User> users = new ArrayList<>();
 
-    private static int userCount = 0; 
+    private static int userCount = 0;
 
     static {
-        users.add(new User(++userCount,"Ahtesam",LocalDate.now().minusYears(25)));
-        users.add(new User(++userCount,"Pablo",LocalDate.now().minusYears(30)));
-        users.add(new User(++userCount,"Miguel",LocalDate.now().minusYears(40)));
+        users.add(new User(++userCount, "Ahtesam", LocalDate.now().minusYears(25)));
+        users.add(new User(++userCount, "Pablo", LocalDate.now().minusYears(30)));
+        users.add(new User(++userCount, "Miguel", LocalDate.now().minusYears(40)));
     }
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return users;
     }
 
@@ -31,10 +31,14 @@ public class UserDaoService {
         users.add(user);
         return user;
     }
-    
-    public User findOne(int id){
-        Predicate<? super User> predicate = user -> 
-            user.getId().equals(id);
-        return users.stream().filter(predicate).findFirst().get();
+
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().orElse(null);
+    }
+
+    public void deleteById(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        users.removeIf(predicate);
     }
 }
